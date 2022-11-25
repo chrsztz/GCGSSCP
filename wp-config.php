e2e669f1662aea74896bf920a60d5a1013d97ee1
chrsztz <118425500+chrsztz@users.noreply.github.com>
2022-11-25 19:47:39 +0800

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'bgyservice_cn' );
+define( 'DB_NAME', '' );
 
 /** Database username */
-define( 'DB_USER', 'bgyservice_cn' );
+define( 'DB_USER', '' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'PRJeexDstYZ3YyiG' );
+define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
